ba960bcc7a83ee77b0d72ecd5714faee4c030f01
poojanavgurukul <pooja17@navgurukul.org>
2021-05-05 18:40:32 +0530

    added home and header component

---
+++ bridge_connectivity_task/src/Component/Home/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../Header/Header.js';
+import '../Home/Homepage.css'
+function HomeComponent(){
+    return(
+        <div class="box-center">
+            <Header />
+        </div>
+    )
+}
+export default HomeComponent;
